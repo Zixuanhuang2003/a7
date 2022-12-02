eecfb4e24100b81c7f947c0516244613a8e89adb
YenHsing <31437399+YenHsing@users.noreply.github.com>
2022-12-01 19:02:30 -0500

    update McDiver.java, use PQueue in seek phase

--- src/diver/McDiver.java
+++ src/diver/McDiver.java
@@ -1,5 +1,8 @@
 package diver;
 
+import cms.util.maybe.Maybe;
+import datastructures.PQueue;
+import datastructures.SlowPQueue;
 import game.*;
 import graph.ShortestPaths;
 import java.util.ArrayList;
@@ -19,44 +22,30 @@ public class McDiver implements SewerDiver {
     /** See {@code SewerDriver} for specification. */
     @Override
     public void seek(SeekState state) {
-        // TODO : Look for the ring and return.
-        // DO NOT WRITE ALL THE CODE HERE. DO NOT MAKE THIS METHOD RECURSIVE.
-        // Instead, write your method (it may be recursive) elsewhere, with a
-        // good specification, and call it from this one.
-        //
-        // Working this way provides you with flexibility. For example, write
-        // one basic method, which always works. Then, make a method that is a
-        // copy of the first one and try to optimize in that second one.
-        // If you don't succeed, you can always use the first one.
-        //
-        // Use this same process on the second method, scram.
         dfs_search(state);
     }
 
     /** See {@code SewerDriver} for specification. */
     @Override
     public void scram(ScramState state) {
-        // TODO: Get out of the sewer system before the steps are used up.
-        // DO NOT WRITE ALL THE CODE HERE. Instead, write your method elsewhere,
-        // with a good specification, and call it from this one.
         scram_route(state);
-
     }
 
 
     public void dfs_search(SeekState state){
         long id = 0;
         long cur = state.currentLocation();
-        ArrayList<NodeStatus> neighbor_list = new ArrayList<>();
+        PQueue<NodeStatus> neighbor_list_pq = new SlowPQueue<>();
         visited_nodes.put(cur, true);
         if (state.distanceToRing()!=0){
             for(NodeStatus ns : state.neighbors()){
                 id = ns.getId();
                 if(!visited_nodes.containsKey(id)) visited_nodes.put(id, false);
-                neighbor_list.add(ns);
+                neighbor_list_pq.add(ns, ns.getDistanceToRing());
             }
-            for(NodeStatus node_stat : neighbor_list){
-                if(visited_nodes.get(node_stat.getId())==false) {
+            while(!neighbor_list_pq.isEmpty()){
+                NodeStatus node_stat = neighbor_list_pq.extractMin();
+                if(visited_nodes.get(node_stat.getId())==false){
                     state.moveTo(node_stat.getId());
                     seek(state);
                     if(state.distanceToRing() == 0) return;
@@ -73,6 +62,7 @@ public class McDiver implements SewerDiver {
             nodes.add(n);
         }
         Maze escape_route = new Maze(Collections.unmodifiableSet(new HashSet<>(nodes)));
+        //shorttest path
         ShortestPaths<Node, Edge> dijkstra = new ShortestPaths<>(escape_route);
         dijkstra.singleSourceDistances(state.currentNode());
         List<Edge> route = dijkstra.bestPath(state.exit());
